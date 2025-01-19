86f89cd63e44a7d2d83bf069e070bf50ed7cbd92
l0n3r <mirshohid1214@gmail.com>
2025-01-19 17:53:14 +0500

    feat: update CustomUser model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,17 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from django.core.exceptions import ValidationError
 
-import re
+from config.validators import validate_phone_number, data_format_validate
 
-
-def validate_phone_number(value):
-    pattern = r'^(?:\+998\d{9}|\+7\d{10})$'
-    if not re.match(pattern, value):
-        raise ValidationError(
-            "The phone number must start with '+998' (Uzbekistan) or '+7' (Russia) and contain the correct number of digits."
-        )
 
 def path_to_avatar(instance, filename):
     return f"uploads/user_{instance.id}/avatar_{filename}"
@@ -26,6 +18,17 @@ class CustomUser(AbstractUser):
         validators=[validate_phone_number],
     )
     avatar = models.ImageField(upload_to=path_to_avatar, blank=True, null=True)
+
+    def clean(self):
+        self.first_name = data_format_validate(self.first_name, is_name=True)
+        self.last_name = data_format_validate(self.last_name, is_name=True)
+        self.phone_number = data_format_validate(self.phone_number, unique=True, required=True)
+        self.email = data_format_validate(self.email, unique=True, required=True)
+        self.username = data_format_validate(self.username, unique=True, required=True)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'User'
