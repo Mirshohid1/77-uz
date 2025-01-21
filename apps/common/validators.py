a6ec7052f691ec258da074e813354e3fc99c27f5
l0n3r <mirshohid1214@gmail.com>
2025-01-21 19:56:44 +0500

    none: nmaga teyganim esdan chiqdi

--- apps/common/validators.py
+++ apps/common/validators.py
@@ -17,15 +17,21 @@ def validate_phone_number(value):
         )
 
 
-def data_format_validate(value: str, is_name=False, required=False, unique=False):
+def data_format_validate(
+        value: str, title=False, capitalize=False,
+        required=False, unique=False
+):
     """
     Function to validate and format a string based on the given parameters.
 
     :param value: the string to be validated and formatted.
-    :param is_name: boolean flag indicating if the string is a name. If True, the string is formatted with title case.
+    :param title: boolean flag indicating if the string should be in title case. If True, the string is formatted with title case.
+    :param capitalize: boolean flag indicating if the string should be capitalized. If True, the string is formatted with capitalize case.
     :param required: boolean flag indicating if the string is required. If True and the string is empty, raises a ValidationError.
     :param unique: boolean flag indicating if the string should be unique. If True, the string is converted to lowercase.
     :return: the formatted string.
+
+    Note: It is not recommended to enable both `title` and `capitalize` at the same time.
     """
 
     if not value.strip() and required:
@@ -33,8 +39,10 @@ def data_format_validate(value: str, is_name=False, required=False, unique=False
             _("This field is required.")
         )
 
-    if is_name and not unique:
+    if title and not unique:
         return value.strip().title()
+    elif capitalize and not unique:
+        return value.strip().capitalize()
     elif unique:
         return value.strip().lower()
     else:
