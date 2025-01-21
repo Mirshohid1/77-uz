2b91193325d1efc16c3e9ad0a1e2964f291413d4
l0n3r <mirshohid1214@gmail.com>
2025-01-21 11:14:34 +0500

    feat: add Address model

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    street = models.CharField(max_length=100, null=True, blank=True)
+    building_number = models.CharField(max_length=10, null=True, blank=True)
+    apartment_number = models.CharField(max_length=10, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, null=True, blank=True)
+    additional_info = models.TextField(blank=True, null=True)
+
+    def get_short_address(self):
+        return f"{self.country}, {self.city}, {self.district}"
